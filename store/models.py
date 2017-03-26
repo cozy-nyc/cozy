048a66b26fcfd8342e25b6202b1a4ff43c3e5ce3
Rantahu <rantahu@gmail.com>
2017-03-26 18:49:52 -0400

    small edit

--- store/models.py
+++ store/models.py
@@ -16,3 +16,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length=32)
+
+    def _str_(self):
+        return self.name
