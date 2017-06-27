8e6a4748ce5f572b49b8f58bf475f2f43bcc927c
Rokney Rosemie <rantahu@gmail.com>
2017-06-26 22:48:05 -0400

    fixed /admin login

--- cuser/forms.py
+++ cuser/forms.py
@@ -5,15 +5,15 @@ from cuser.models import User
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-    displayName = forms.CharField(max_length=40, help_text='Required.')
+    username = forms.CharField(max_length=40, help_text='Required.')
 
     class Meta:
         model = User
-        fields = ('email', 'displayName', 'password1', 'password2')
+        fields = ('email', 'username', 'password1', 'password2')
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
-        displayName = self.cleaned_data.get('displayName')
-        if email and User.objects.filter(displayName=displayName).exclude(email=email).exists():
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(username=username).exclude(email=email).exists():
             raise forms.ValidationError(u'Display Name must be unique.')
         return email
