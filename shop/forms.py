8b0531f6bc1c0b1656ab1818f1751343763a54e4
Rantahu <rantahu@gmail.com>
2017-07-14 01:49:39 -0400

    Added documentation to 'shop' app

--- shop/forms.py
+++ shop/forms.py
@@ -3,6 +3,8 @@ from django.forms import ModelForm
 from .models import Listing, Item
 
 class PostListingForm(ModelForm):
+    """This is a form for users to create listings
+    """
     item_name = forms.CharField(max_length=200)
     item_category = forms.CharField(max_length=16)
     item_subCategory = forms.CharField(max_length=16)
@@ -15,6 +17,12 @@ class PostListingForm(ModelForm):
         fields=['item_name', 'item_category', 'item_subCategory', 'size', 'conditionRating', 'description','price', 'location']
 
     def FindItem(self):
+        """This function checks if a item exist
+
+        If the 'item_name' matches or is similar to an existing 'item' in the database
+        than that item will be suggested and auto fill the rest of item info.
+        Else calls 'CreateItem()'
+        """
         # If user pick a other as suggestion then CreateItem() with item name
         # Else
             # If there's a item similar to the item_name given then suggest that item(s)
@@ -22,4 +30,9 @@ class PostListingForm(ModelForm):
         return
 
     def CreateItem(self):
+        """This function creates an item
+
+        Note:
+            Might move this function
+        """
         return
