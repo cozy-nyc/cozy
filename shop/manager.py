ac124e20475ad3228715dd74c6553b37b5dd81ca
juan <jj1913@nyu.edu>
2017-07-18 20:27:10 -0400

    adding comments

--- shop/manager.py
+++ shop/manager.py
@@ -41,30 +41,90 @@ class ListingManager(Manager):
     as well as functions that will be used in our django project
     """
     def avgSoldPrice(self,itemRef):
+        """
+            This function will run a query that will return the average price
+            that a paticular item has been sold for.
+
+            Args:
+                self: current instance of that object
+                itemRef: the reference to the item we wish to find the average
+                    selling price of
+
+            Return: An float that contains the average sold price of a
+                        particular item
+        """
         return self.filter(
             item = itemRef,
             available = False
             ).aggregate(Avg('price'))
 
     def lowestSoldPrice(self, itemRef):
+        """
+            This function will run a query that will return the lowest price
+            that a paticular item has been sold for.
+
+            Args:
+                self: current instance of that object
+                itemRef: the reference to the item we wish to find the lowest
+                    sold price of
+
+            Return: An float that contains the lowest sold price of a
+                        particular item
+        """
         return self.filter(
             item = itemRef,
             available = False
             ).aggregate(Min('price'))
 
     def highestSoldPrice(self, itemRef):
+        """
+            This function will run a query that will return the highest price
+            that a paticular item has been sold for.
+
+            Args:
+                self: current instance of that object
+                itemRef: the reference to the item we wish to find the highest
+                    selling price of
+
+            Return: An float that contains the lowest sold price of a
+                        particular item
+        """
         return self.filter(
             item = itemRef,
             available = False
             ).aggregate(Max('price'))
 
     def lowestCurrentPrice(self):
+        """
+            This function will run a query that will return the lowest price
+            that is currently listed
+
+            Args:
+                self: current instance of that object
+                itemRef: the reference to the item we wish to find the lowest
+                            current listing price
+
+            Return: An float that contains the lowest current listing price of
+                        a particular item
+        """
         return self.filter(
             item = itemRef,
             available = True
             ).aggregate(Min('price'))
 
     def highestCurrentPrice(self):
+        """
+            This function will run a query that will return the highest price
+            that is currently listed
+
+            Args:
+                self: current instance of that object
+                itemRef: the reference to the item we wish to find the highest
+                            current listing price
+
+            Return: An float that contains the highest current listing price of
+                        a particular item
+        """
         return self.filter(
             item = itemRef,
             available = True
